ff1da3bffdba5e98edcbffd36fc33eaa3068ac9f
EdgarsLv <edgars0707@gmail.com>
2025-08-17 22:50:36 +0300

    add images

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -2,6 +2,8 @@
 import { initializeApp } from 'firebase/app';
 import { getAnalytics, isSupported } from 'firebase/analytics';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,7 +11,7 @@ import { getAuth } from 'firebase/auth';
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyCoOaQYv3pYwArvlGy-45zPSZikH6OeSbs',
-  authDomain: 'mindareas.com', //sisiquiz.firebaseapp.com
+  authDomain: 'sisiquiz.firebaseapp.com', //sisiquiz.firebaseapp.com
   projectId: 'sisiquiz',
   storageBucket: 'sisiquiz.firebasestorage.app',
   messagingSenderId: '327586798593',
@@ -33,5 +35,9 @@ if (typeof window !== 'undefined') {
 }
 
 const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, analytics, auth, db, storage };
 
 export { app, auth, analytics };
