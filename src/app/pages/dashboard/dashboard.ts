c31efdc0e6faa2bce678139abf0cd18d3f56777f
EdgarsLv <edgars0707@gmail.com>
2025-08-20 00:11:16 +0300

    change home and dashboard

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,20 +1,78 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { Avatar } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
+import { AuthService } from '../../services/auth.service';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { filter, Subject, takeUntil } from 'rxjs';
+import { Message } from 'primeng/message';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Avatar, ButtonModule],
+  imports: [Avatar, ButtonModule, Message],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {
+export class Dashboard implements OnDestroy {
+  public authService = inject(AuthService);
   private router = inject(Router);
+
+  public init = signal<boolean>(true);
+  public canTakeTest = signal<boolean>(false);
+  public countdown = signal<string>('');
+  private timerId: any;
 
   public items = ['IQ Test', 'Sociotype Test', 'Love language test', 'Coming soon'];
 
+  private destroy$ = new Subject<void>();
+
+  constructor() {
+    toObservable(this.authService.profile)
+      .pipe(
+        filter((val) => val !== null),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((time) => {
+        this.init.set(false);
+        if (time?.testTakenAt) {
+          const date = time.testTakenAt;
+          this.timerId = setInterval(() => this.updateCountdown(date), 1000);
+        }
+      });
+  }
+
   public navigateToIqTest(): void {
     this.router.navigate(['iq-test']);
+  }
+
+  private updateCountdown(time: string): void {
+    const now = new Date().getTime();
+    const target = new Date(time).getTime() + 24 * 60 * 60 * 1000; // +24h
+    const diff = target - now;
+
+    if (diff <= 0) {
+      this.canTakeTest.set(true);
+      this.countdown.set('');
+      clearInterval(this.timerId); // stop ticking
+    } else {
+      this.canTakeTest.set(false);
+      this.countdown.set(this.formatTime(diff));
+    }
+  }
+
+  private formatTime(ms: number): string {
+    const totalSeconds = Math.floor(ms / 1000);
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = totalSeconds % 60;
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds
+      .toString()
+      .padStart(2, '0')}`;
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+    clearInterval(this.timerId);
   }
 }
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,13 +2,12 @@ import { Component, inject, OnDestroy, signal } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
-import { BehaviorSubject, filter, Subject, takeUntil } from 'rxjs';
-import { Message } from 'primeng/message';
+import { filter, Subject, takeUntil } from 'rxjs';
 import { toObservable } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonModule, Message],
+  imports: [ButtonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
@@ -19,11 +18,7 @@ export class Home implements OnDestroy {
   public user = this.authService.authUser;
 
   public init = signal<boolean>(true);
-  public canTakeTest = signal<boolean>(false);
-  public countdown = signal<string>('');
-  private timerId: any;
 
-  public countDownSubject$ = new BehaviorSubject<string | undefined>(undefined);
   private destroy$ = new Subject<void>();
 
   constructor() {
@@ -32,17 +27,13 @@ export class Home implements OnDestroy {
         filter((val) => val !== null),
         takeUntil(this.destroy$)
       )
-      .subscribe((time) => {
+      .subscribe(() => {
         this.init.set(false);
-        if (time?.testTakenAt) {
-          const date = time.testTakenAt;
-          this.timerId = setInterval(() => this.updateCountdown(date), 1000);
-        }
       });
   }
 
   public startTest(): void {
-    this.router.navigate(['iq-test']);
+    this.router.navigate(['dashboard']);
   }
 
   public async loginWithGoogle(): Promise<void> {
@@ -53,34 +44,8 @@ export class Home implements OnDestroy {
     }
   }
 
-  private updateCountdown(time: string): void {
-    const now = new Date().getTime();
-    const target = new Date(time).getTime() + 24 * 60 * 60 * 1000; // +24h
-    const diff = target - now;
-
-    if (diff <= 0) {
-      this.canTakeTest.set(true);
-      this.countdown.set('');
-      clearInterval(this.timerId); // stop ticking
-    } else {
-      this.canTakeTest.set(false);
-      this.countdown.set(this.formatTime(diff));
-    }
-  }
-
-  private formatTime(ms: number): string {
-    const totalSeconds = Math.floor(ms / 1000);
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = totalSeconds % 60;
-    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds
-      .toString()
-      .padStart(2, '0')}`;
-  }
-
   public ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
-    clearInterval(this.timerId);
   }
 }
