538a63b8a42eb68be47934a19e6b8c91ba6de8ba
EdgarsLv <edgars0707@gmail.com>
2025-08-19 20:28:35 +0300

    add navigation

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { Avatar } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 
@@ -9,5 +10,11 @@ import { ButtonModule } from 'primeng/button';
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
+  private router = inject(Router);
+
   public items = ['IQ Test', 'Sociotype Test', 'Love language test', 'Coming soon'];
+
+  public navigateToIqTest(): void {
+    this.router.navigate(['iq-test']);
+  }
 }
