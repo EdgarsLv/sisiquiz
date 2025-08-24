5205e79afacf80f168aee6f7bf4ab01cc08f01bd
EdgarsLv <edgars0707@gmail.com>
2025-08-24 19:10:25 +0300

    remove logo from footer

--- src/app/layout/components/footer/footer.ts
+++ src/app/layout/components/footer/footer.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { Logo } from '../../../components/logo/logo';
 
 @Component({
   selector: 'app-footer',
-  imports: [Logo],
+  imports: [],
   templateUrl: './footer.html',
   styleUrl: './footer.scss',
 })
