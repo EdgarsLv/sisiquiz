4f08d45469cfb4776e7fa91801dbc2864c575f9d
EdgarsLv <edgars0707@gmail.com>
2025-08-24 21:49:49 +0300

    dashboard styling

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -2,10 +2,7 @@ import { Component, inject, OnDestroy, signal } from '@angular/core';
 import { Router } from '@angular/router';
 import { Avatar } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
-import { AuthService } from '../../services/auth.service';
-import { toObservable } from '@angular/core/rxjs-interop';
-import { filter, Subject, takeUntil } from 'rxjs';
-import { Message } from 'primeng/message';
+import { interval, Subject, takeUntil } from 'rxjs';
 import { StorageService } from '../../services/storage.service';
 
 type AvailableTests = {
@@ -44,73 +41,39 @@ const tests: AvailableTests[] = [
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Avatar, ButtonModule, Message],
+  imports: [Avatar, ButtonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard implements OnDestroy {
-  private authService = inject(AuthService);
   private router = inject(Router);
   private storageService = inject(StorageService);
 
-  public init = signal<boolean>(true);
-  public canTakeTest = signal<boolean>(false);
-  public countdown = signal<string>('');
-  private timerId: any;
+  public countdown = signal<string | null>(null);
 
   public availableTests = signal<AvailableTests[]>(tests);
 
   private destroy$ = new Subject<void>();
 
   constructor() {
-    toObservable(this.authService.profile)
-      .pipe(
-        filter((val) => val !== null),
-        takeUntil(this.destroy$)
-      )
-      .subscribe((time) => {
-        this.init.set(false);
-        if (time?.testTakenAt) {
-          const date = time.testTakenAt;
-          this.timerId = setInterval(() => this.updateCountdown(date), 1000);
-        }
-      });
+    this.startCountdown();
+  }
 
-    console.log('love', this.storageService.getCountdown('love'));
+  private startCountdown(): void {
+    interval(1000)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(() => {
+        const countdown = this.storageService.getCountdown('love');
+        this.countdown.set(countdown);
+      });
   }
 
   public navigateToTest(link: string): void {
     this.router.navigate([link]);
   }
 
-  private updateCountdown(time: string): void {
-    const now = new Date().getTime();
-    const target = new Date(time).getTime() + 24 * 60 * 60 * 1000; // +24h
-    const diff = target - now;
-
-    if (diff <= 0) {
-      this.canTakeTest.set(true);
-      this.countdown.set('');
-      clearInterval(this.timerId); // stop ticking
-    } else {
-      this.canTakeTest.set(false);
-      this.countdown.set(this.formatTime(diff));
-    }
-  }
-
-  private formatTime(ms: number): string {
-    const totalSeconds = Math.floor(ms / 1000);
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = totalSeconds % 60;
-    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds
-      .toString()
-      .padStart(2, '0')}`;
-  }
-
   public ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
-    clearInterval(this.timerId);
   }
 }
